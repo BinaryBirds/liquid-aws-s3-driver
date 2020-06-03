1792ed95249330d24a04872bec709f1fcd876c68
Michael Redig <mredig@gmail.com>
2020-06-03 17:29:31 -0500

    (feat) wrote test to confirm that custom endpoint urls are generated correctly

--- Tests/LiquidAwsS3DriverTests/LiquidAwsS3DriverTests.swift
+++ Tests/LiquidAwsS3DriverTests/LiquidAwsS3DriverTests.swift
@@ -5,10 +5,11 @@ final class LiquidAwsS3DriverTests: XCTestCase {
     
     let key = "****"
     let secret = "****"
-    let bucket = "****"
+    let bucket = "bucket"
     let region = Region.uswest1
+    let customEndpoint = "https://s3.wasabisys.com/"
 
-    private func createTestStorage() -> FileStorage {
+    private func createTestStorage(withEndpoint endpoint: String? = nil) -> FileStorage {
         let eventLoop = EmbeddedEventLoop()
         let storages = FileStorages(fileio: .init(threadPool: .init(numberOfThreads: 1)))
         storages.use(.awsS3(key: self.key, secret: self.secret, bucket: self.bucket, region: self.region), as: .awsS3)
@@ -25,5 +26,13 @@ final class LiquidAwsS3DriverTests: XCTestCase {
         let data = Data("file storage test".utf8)
         let res = try fs.upload(key: key, data: data).wait()
         XCTAssertEqual(res, "https://\(self.bucket).s3-\(self.region.rawValue).amazonaws.com/\(key)")
+    }
+
+    func testCustomEndpointUpload() throws {
+        let fs = self.createTestStorage(withEndpoint: customEndpoint)
+        let key = "test"
+        let data = Data("file storage test".utf8)
+        let res = try fs.upload(key: key, data: data).wait()
+        XCTAssertEqual(res, "\(self.customEndpoint)\(self.bucket)/\(key)")
     }
 }
