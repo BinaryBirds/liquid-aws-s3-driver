9a2aa4aa7c1919151a49c141e69b98e1c03205fc
Tibor Bödecs <mail.tib@gmail.com>
2021-12-14 14:36:53 +0100

    Dep updates

--- Package.swift
+++ Package.swift
@@ -1,16 +1,16 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
     name: "liquid-aws-s3-driver",
     platforms: [
-       .macOS(.v10_15)
+       .macOS(.v12)
     ],
     products: [
         .library(name: "LiquidAwsS3Driver", targets: ["LiquidAwsS3Driver"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/binarybirds/liquid-kit.git", from: "1.3.1"),
+        .package(url: "https://github.com/binarybirds/liquid-kit.git", from: "1.3.2"),
         .package(url: "https://github.com/soto-project/soto.git", from: "5.11.0")
     ],
     targets: [
